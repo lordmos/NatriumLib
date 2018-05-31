8e895f0951b61210494d5f1b259b9139978e0261
Hui Zhong <xanogayu@live.com>
2018-05-31 11:31:29 +0800

    修复na-pagination图标不展示的问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	currentPage = 0;
+	totalPage = 100;
+	pageTo(page: number) {
+		console.log(page)
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NatriumLibModule } from 'dist/natrium-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NatriumLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
